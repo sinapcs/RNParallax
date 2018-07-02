d1092588e34708c9248998b68fe339e4598fcedb
Sina <sina.pcs@gmail.com>
2018-07-03 01:49:59 +0430

    Custom header added + zIndex priority

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {
-  StyleSheet,
-  Platform,
-  Animated,
-  Text,
-  View,
-  Dimensions,
+    StyleSheet,
+    Platform,
+    ScrollView,
+    Animated,
+    Text,
+    View,
+    Dimensions,
 } from 'react-native';
 
 const {
-  height: SCREEN_HEIGHT,
+    height: SCREEN_HEIGHT,
 } = Dimensions.get('window');
 
 const IS_IPHONE_X = SCREEN_HEIGHT === 812;
@@ -28,314 +29,321 @@ const DEFAULT_BACKGROUND_COLOR = '#303F9F';
 const DEFAULT_TITLE_COLOR = 'white';
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    flex: 1,
-  },
-  scrollView: {
-    flex: 1,
-  },
-  header: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    backgroundColor: DEFAULT_NAVBAR_COLOR,
-    overflow: 'hidden',
-  },
-  backgroundImage: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    width: null,
-    height: DEFAULT_HEADER_MAX_HEIGHT,
-    resizeMode: 'cover',
-  },
-  bar: {
-    backgroundColor: 'transparent',
-    height: DEFAULT_HEADER_MIN_HEIGHT,
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-  },
-  headerTitle: {
-    backgroundColor: 'transparent',
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    paddingTop: STATUS_BAR_HEIGHT,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  headerText: {
-    color: DEFAULT_TITLE_COLOR,
-    textAlign: 'center',
-    fontSize: 16,
-  },
+    container: {
+        backgroundColor: 'white',
+        flex: 1,
+    },
+    scrollView: {
+        flex: 1,
+    },
+    header: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: DEFAULT_NAVBAR_COLOR,
+        overflow: 'hidden',
+        zIndex:1
+    },
+    backgroundImage: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        width: null,
+        height: DEFAULT_HEADER_MAX_HEIGHT,
+        resizeMode: 'cover',
+    },
+    bar: {
+        backgroundColor: 'transparent',
+        height: DEFAULT_HEADER_MIN_HEIGHT,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        zIndex:2
+    },
+    headerTitle: {
+        backgroundColor: 'transparent',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        paddingTop: STATUS_BAR_HEIGHT,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    headerText: {
+        color: DEFAULT_TITLE_COLOR,
+        textAlign: 'center',
+        fontSize: 16,
+    },
 });
 
 class RNParallax extends Component {
-  constructor() {
-    super();
-    this.state = {
-      scrollY: new Animated.Value(0),
-    };
-  }
+    constructor() {
+        super();
+        this.state = {
+            scrollY: new Animated.Value(0),
+        };
+    }
 
-  getHeaderMaxHeight() {
-    const { headerMaxHeight } = this.props;
-    return headerMaxHeight;
-  }
+    getHeaderMaxHeight() {
+        const {headerMaxHeight} = this.props;
+        return headerMaxHeight;
+    }
 
-  getHeaderMinHeight() {
-    const { headerMinHeight } = this.props;
-    return headerMinHeight;
-  }
+    getHeaderMinHeight() {
+        const {headerMinHeight} = this.props;
+        return headerMinHeight;
+    }
 
-  getHeaderScrollDistance() {
-    return this.getHeaderMaxHeight() - this.getHeaderMinHeight();
-  }
+    getHeaderScrollDistance() {
+        return this.getHeaderMaxHeight() - this.getHeaderMinHeight();
+    }
 
-  getExtraScrollHeight() {
-    const { extraScrollHeight } = this.props;
-    return extraScrollHeight;
-  }
+    getExtraScrollHeight() {
+        const {extraScrollHeight} = this.props;
+        return extraScrollHeight;
+    }
 
-  getBackgroundImageScale() {
-    const { backgroundImageScale } = this.props;
-    return backgroundImageScale;
-  }
+    getBackgroundImageScale() {
+        const {backgroundImageScale} = this.props;
+        return backgroundImageScale;
+    }
 
-  getInputRange() {
-    return [-this.getExtraScrollHeight(), 0, this.getHeaderScrollDistance()];
-  }
+    getInputRange() {
+        return [-this.getExtraScrollHeight(), 0, this.getHeaderScrollDistance()];
+    }
 
-  getHeaderHeight() {
-    return this.state.scrollY.interpolate({
-      inputRange: this.getInputRange(),
-      outputRange: [this.getHeaderMaxHeight() + this.getExtraScrollHeight(), this.getHeaderMaxHeight(), this.getHeaderMinHeight()],
-      extrapolate: 'clamp',
-    });
-  }
+    getHeaderHeight() {
+        return this.state.scrollY.interpolate({
+            inputRange: this.getInputRange(),
+            outputRange: [this.getHeaderMaxHeight() + this.getExtraScrollHeight(), this.getHeaderMaxHeight(), this.getHeaderMinHeight()],
+            extrapolate: 'clamp',
+        });
+    }
 
-  getNavBarOpacity() {
-    return this.state.scrollY.interpolate({
-      inputRange: this.getInputRange(),
-      outputRange: [0, 1, 1],
-      extrapolate: 'clamp',
-    });
-  }
+    getNavBarOpacity() {
+        return this.state.scrollY.interpolate({
+            inputRange: this.getInputRange(),
+            outputRange: [0, 1, 1],
+            extrapolate: 'clamp',
+        });
+    }
 
-  getImageOpacity() {
-    return this.state.scrollY.interpolate({
-      inputRange: this.getInputRange(),
-      outputRange: [1, 1, 0],
-      extrapolate: 'clamp',
-    });
-  }
+    getImageOpacity() {
+        return this.state.scrollY.interpolate({
+            inputRange: this.getInputRange(),
+            outputRange: [1, 1, 0],
+            extrapolate: 'clamp',
+        });
+    }
 
-  getImageTranslate() {
-    return this.state.scrollY.interpolate({
-      inputRange: this.getInputRange(),
-      outputRange: [0, 0, -50],
-      extrapolate: 'clamp',
-    });
-  }
+    getImageTranslate() {
+        return this.state.scrollY.interpolate({
+            inputRange: this.getInputRange(),
+            outputRange: [0, 0, -50],
+            extrapolate: 'clamp',
+        });
+    }
 
-  getImageScale() {
-    return this.state.scrollY.interpolate({
-      inputRange: this.getInputRange(),
-      outputRange: [this.getBackgroundImageScale(), 1, 1],
-      extrapolate: 'clamp',
-    });
-  }
+    getImageScale() {
+        return this.state.scrollY.interpolate({
+            inputRange: this.getInputRange(),
+            outputRange: [this.getBackgroundImageScale(), 1, 1],
+            extrapolate: 'clamp'
+        });
+    }
 
-  getTitleTranslate() {
-    return this.state.scrollY.interpolate({
-      inputRange: this.getInputRange(),
-      outputRange: [5, 0, 0],
-      extrapolate: 'clamp',
-    });
-  }
+    getTitleTranslate() {
+        return this.state.scrollY.interpolate({
+            inputRange: this.getInputRange(),
+            outputRange: [5, 0, 0],
+            extrapolate: 'clamp',
+        });
+    }
 
-  renderHeaderTitle() {
-    const { title, titleStyle } = this.props;
-    const titleTranslate = this.getTitleTranslate();
+    renderHeaderTitle() {
+        const {title, titleStyle} = this.props;
+        const titleTranslate = this.getTitleTranslate();
 
-    return (
-      <Animated.View
-        style={[
-          styles.headerTitle,
-          {
-            transform: [
-              { translateY: titleTranslate },
-            ],
-            height: this.getHeaderHeight(),
-          },
-        ]}
-      >
-        <Text style={[styles.headerText, titleStyle]}>
-          {title}
-        </Text>
-      </Animated.View>
-    );
-  }
+        return (
+            <Animated.View
+                style={[
+                    styles.headerTitle,
+                    {
+                        transform: [
+                            {translateY: titleTranslate},
+                        ],
+                        height: this.getHeaderHeight(),
+                    },
+                ]}
+            >
+                <Text style={[styles.headerText, titleStyle]}>
+                    {title}
+                </Text>
+            </Animated.View>
+        );
+    }
 
-  renderHeaderForeground() {
-    const { renderNavBar } = this.props;
+    renderHeaderForeground() {
+        const {renderNavBar} = this.props;
 
-    return (
-      <Animated.View
-        style={[
-          styles.bar,
-          {
-            height: this.getHeaderMinHeight(),
-          },
-        ]}
-      >
-        {renderNavBar()}
-      </Animated.View>
-    );
-  }
+        return (
+            <Animated.View
+                style={[
+                    styles.bar,
+                    {
+                        height: this.getHeaderMinHeight(),
+                    }
+                ]}
+            >
+                {renderNavBar()}
+            </Animated.View>
+        );
+    }
 
-  renderBackgroundImage() {
-    const { backgroundImage } = this.props;
-    const imageOpacity = this.getImageOpacity();
-    const imageTranslate = this.getImageTranslate();
-    const imageScale = this.getImageScale();
+    renderBackgroundImage() {
+        const {backgroundImage} = this.props;
+        const imageOpacity = this.getImageOpacity();
+        const imageTranslate = this.getImageTranslate();
+        const imageScale = this.getImageScale();
+        return (
+            <Animated.View
+                style={[
+                    styles.backgroundImage,
+                    {
+                        height: this.getHeaderMaxHeight(),
+                        opacity: imageOpacity,
+                        transform: [{translateY: imageTranslate}, {scale: imageScale}],
+                    },
+                ]}
+            >
+                {backgroundImage}
+            </Animated.View>
+        )
+    }
 
-    return (
-      <Animated.Image
-        style={[
-          styles.backgroundImage,
-          {
-            height: this.getHeaderMaxHeight(),
-            opacity: imageOpacity,
-            transform: [{ translateY: imageTranslate }, { scale: imageScale }],
-          },
-        ]}
-        source={backgroundImage}
-      />
-    );
-  }
+    renderPlainBackground() {
+        const {backgroundColor} = this.props;
 
-  renderPlainBackground() {
-    const { backgroundColor } = this.props;
+        const imageOpacity = this.getImageOpacity();
+        const imageTranslate = this.getImageTranslate();
+        const imageScale = this.getImageScale();
 
-    const imageOpacity = this.getImageOpacity();
-    const imageTranslate = this.getImageTranslate();
-    const imageScale = this.getImageScale();
+        return (
+            <Animated.View
+                style={{
+                    height: this.getHeaderMaxHeight(),
+                    backgroundColor,
+                    opacity: imageOpacity,
+                    transform: [{translateY: imageTranslate}, {scale: imageScale}],
+                }}
+            />
+        );
+    }
 
-    return (
-      <Animated.View
-        style={{
-          height: this.getHeaderMaxHeight(),
-          backgroundColor,
-          opacity: imageOpacity,
-          transform: [{ translateY: imageTranslate }, { scale: imageScale }],
-        }}
-      />
-    );
-  }
+    renderNavbarBackground() {
+        const {navbarColor} = this.props;
+        const navBarOpacity = this.getNavBarOpacity();
 
-  renderNavbarBackground() {
-    const { navbarColor } = this.props;
-    const navBarOpacity = this.getNavBarOpacity();
+        return (
+            <Animated.View
+                style={[
+                    styles.header,
+                    {
+                        height: this.getHeaderHeight(),
+                        backgroundColor: navbarColor,
+                        opacity: navBarOpacity,
+                    },
+                ]}
+            />
+        );
+    }
 
-    return (
-      <Animated.View
-        style={[
-          styles.header,
-          {
-            height: this.getHeaderHeight(),
-            backgroundColor: navbarColor,
-            opacity: navBarOpacity,
-          },
-        ]}
-      />
-    );
-  }
+    renderHeaderBackground() {
+        const {backgroundImage, backgroundColor} = this.props;
+        const imageOpacity = this.getImageOpacity();
 
-  renderHeaderBackground() {
-    const { backgroundImage, backgroundColor } = this.props;
-    const imageOpacity = this.getImageOpacity();
+        return (
+            <Animated.View
+                style={[
+                    styles.header,
+                    {
+                        height: this.getHeaderHeight(),
+                        opacity: imageOpacity,
+                        backgroundColor: backgroundImage ? 'transparent' : backgroundColor,
+                    },
+                ]}
+            >
+                {this.props.customHeader}
+                {!this.props.customHeader && backgroundImage && this.renderBackgroundImage()}
+                {!this.props.customHeader && !backgroundImage && this.renderPlainBackground()}
+            </Animated.View>
+        );
+    }
 
-    return (
-      <Animated.View
-        style={[
-          styles.header,
-          {
-            height: this.getHeaderHeight(),
-            opacity: imageOpacity,
-            backgroundColor: backgroundImage ? 'transparent' : backgroundColor,
-          },
-        ]}
-      >
-        {backgroundImage && this.renderBackgroundImage()}
-        {!backgroundImage && this.renderPlainBackground()}
-      </Animated.View>
-    );
-  }
+    renderScrollView() {
+        const {renderContent, scrollEventThrottle} = this.props;
 
-  renderScrollView() {
-    const { renderContent, scrollEventThrottle } = this.props;
+        return (
+            <Animated.ScrollView
+                style={styles.scrollView}
+                scrollEventThrottle={scrollEventThrottle}
+                onScroll={Animated.event(
+                    [{nativeEvent: {contentOffset: {y: this.state.scrollY}}}],
+                )}
+            >
+                <View style={{marginTop: this.getHeaderMaxHeight()}}>
+                    {renderContent()}
+                </View>
+            </Animated.ScrollView>
+        );
+    }
 
-    return (
-      <Animated.ScrollView
-        style={styles.scrollView}
-        scrollEventThrottle={scrollEventThrottle}
-        onScroll={Animated.event([{ nativeEvent: { contentOffset: { y: this.state.scrollY } } }])}
-      >
-        <View style={{ marginTop: this.getHeaderMaxHeight() }}>
-          {renderContent()}
-        </View>
-      </Animated.ScrollView>
-    );
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        {this.renderScrollView()}
-        {this.renderNavbarBackground()}
-        {this.renderHeaderBackground()}
-        {this.renderHeaderTitle()}
-        {this.renderHeaderForeground()}
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                {this.renderScrollView()}
+                {this.renderNavbarBackground()}
+                {this.renderHeaderBackground()}
+                {this.renderHeaderTitle()}
+                {this.renderHeaderForeground()}
+            </View>
+        );
+    }
 }
 
 RNParallax.propTypes = {
-  renderNavBar: PropTypes.func,
-  renderContent: PropTypes.func.isRequired,
-  backgroundColor: PropTypes.string,
-  backgroundImage: PropTypes.any,
-  navbarColor: PropTypes.string,
-  title: PropTypes.string,
-  titleStyle: PropTypes.number,
-  headerMaxHeight: PropTypes.number,
-  headerMinHeight: PropTypes.number,
-  scrollEventThrottle: PropTypes.number,
-  extraScrollHeight: PropTypes.number,
-  backgroundImageScale: PropTypes.number,
+    renderNavBar: PropTypes.func,
+    renderContent: PropTypes.func.isRequired,
+    backgroundColor: PropTypes.string,
+    backgroundImage: PropTypes.any,
+    navbarColor: PropTypes.string,
+    title: PropTypes.string,
+    titleStyle: PropTypes.number,
+    headerMaxHeight: PropTypes.number,
+    headerMinHeight: PropTypes.number,
+    scrollEventThrottle: PropTypes.number,
+    extraScrollHeight: PropTypes.number,
+    backgroundImageScale: PropTypes.number,
+    customHeader: PropTypes.any
 };
 
 RNParallax.defaultProps = {
-  renderNavBar: () => <View />,
-  navbarColor: DEFAULT_NAVBAR_COLOR,
-  backgroundColor: DEFAULT_BACKGROUND_COLOR,
-  backgroundImage: null,
-  title: '',
-  titleStyle: styles.headerText,
-  headerMaxHeight: DEFAULT_HEADER_MAX_HEIGHT,
-  headerMinHeight: DEFAULT_HEADER_MIN_HEIGHT,
-  scrollEventThrottle: SCROLL_EVENT_THROTTLE,
-  extraScrollHeight: DEFAULT_EXTRA_SCROLL_HEIGHT,
-  backgroundImageScale: DEFAULT_BACKGROUND_IMAGE_SCALE,
+    renderNavBar: () => <View/>,
+    navbarColor: DEFAULT_NAVBAR_COLOR,
+    backgroundColor: DEFAULT_BACKGROUND_COLOR,
+    backgroundImage: null,
+    title: '',
+    titleStyle: styles.headerText,
+    headerMaxHeight: DEFAULT_HEADER_MAX_HEIGHT,
+    headerMinHeight: DEFAULT_HEADER_MIN_HEIGHT,
+    scrollEventThrottle: SCROLL_EVENT_THROTTLE,
+    extraScrollHeight: DEFAULT_EXTRA_SCROLL_HEIGHT,
+    backgroundImageScale: DEFAULT_BACKGROUND_IMAGE_SCALE,
+    customHeader: null
 };
 
 export default RNParallax;
